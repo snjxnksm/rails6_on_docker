dcc4db27fca4772b511d51de9ee1eeb6122bd406
shinjiNAKASHIMA <12793303+snjxnksm@users.noreply.github.com>
2020-10-05 15:02:53 +0900

    Update rails6_on_docker.sh

--- rails6_on_docker.sh
+++ rails6_on_docker.sh
@@ -52,7 +52,7 @@ cat <<EOF > Gemfile
 source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
-gem 'rails', '~> 6.0.1'
+gem 'rails', '~> 6.0.3'
 EOF
 
 echo "** make Gemfile.lock"
